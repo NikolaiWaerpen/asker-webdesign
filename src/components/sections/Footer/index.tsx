64bd094354b432d8c7f8120f7b3b48c20f66c790
Nikolai Waerpen <nikolaiwaerpen@gmail.com>
2021-04-02 22:22:15 +0200

    logo update

--- src/components/sections/Footer/index.tsx
+++ src/components/sections/Footer/index.tsx
@@ -24,7 +24,10 @@ export default function Footer() {
 
       <div className="text-white space-y-12 px-8 pb-16 pt-12 lg:space-y-10 lg:pt-20 xl:pt-28 md:pb-28 lg:flex lg:justify-between lg:mx-14 xl:mx-48 2xl:mx-96">
         <div className="space-y-12">
-          <img src="images/logo.png" className="mt-3 w-40 md:w-56 lg:w-72" />
+          <img
+            src="images/logosmall.png"
+            className="mt-3 w-40 md:w-56 lg:w-72"
+          />
           <div className="text-white text-md space-y-4">
             <a href={MAIL_TO}>hello@waerpendevelopment.com</a>
             <h5>(+47) 452 93 539</h5>
