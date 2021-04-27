fceb7d8a2c2eb2adddabd219382f01e396ee1226
Nikolai Waerpen <nikolaiwaerpen@gmail.com>
2021-04-27 04:05:01 +0200

    fix wierd scaling bug on icons

--- src/components/CustomHead/index.tsx
+++ src/components/CustomHead/index.tsx
@@ -1,13 +1,14 @@
 import Head from "next/head";
 
 export default function CustomHead() {
+  // return null;
   return (
     <Head>
-      <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
+      {/* <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" /> */}
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
       <meta name="description" content="Asker Webdesign" />
-      <link
+      {/* <link
         rel="apple-touch-icon"
         sizes="180x180"
         href="/apple-touch-icon.png"
@@ -25,14 +26,14 @@ export default function CustomHead() {
         href="/favicon-16x16.png"
       />
       <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" /> */}
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#ffffff" />
       {/* <!--
         manifest.json provides metadata used when your web app is installed on a
         user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
       --> */}
-      <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+      {/* <link rel="manifest" href="%PUBLIC_URL%/manifest.json" /> */}
       {/* <!--
         Notice the use of %PUBLIC_URL% in the tags above.
         It will be replaced with the URL of the `public` folder during the build.
@@ -42,7 +43,7 @@ export default function CustomHead() {
         work correctly both with client-side routing and a non-root public URL.
         Learn how to configure a non-root public URL by running `npm run build`.
       --> */}
-      <link ref="author" href="humans.txt" />
+      {/* <link ref="author" href="humans.txt" /> */}
       <title>Webutvikling og design for småbedrifter | Asker Webdesign</title>
     </Head>
   );
