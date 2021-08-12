7cda3ad60fcd7f578e232b6dadfb18770880c5f5
Nikolai Waerpen <nikolaiwaerpen@gmail.com>
2021-08-12 12:05:19 +0200

    feat: update portfolio data

--- src/components/sections/PortfolioSection/portfolioData.ts
+++ src/components/sections/PortfolioSection/portfolioData.ts
@@ -1,20 +1,28 @@
 export const portfolioData = [
   {
-    image: "/images/showcase/restaurant.png",
-    title: "Eksempel restaurant side",
-    visitTitle: "eksempel",
-    location: "Eksempel",
+    // image: "/images/showcase/restaurant.png",
+    image: "/images/showcase/intro.png",
+    title: "CleanBil AS",
+    visitTitle: "Cleanbil",
+    location: "Oslo og Holmestrand",
     description:
-      "Eksempel side for typisk restaurant bedrift. Inneholder informasjon om bedriften, meny, åpningstider, kontaktinformasjon, link til facebook side og kart.",
-    link: "https://www.waerpen.com/",
+      "CleanBil tilbyr en miljøvennlig bilrengjøring ved bruk at damp. Ingen bruk av såper og kun 2 liter vann benyttes per bil. En ordinær bilvask kan bruke over 450 liter vann per vask, samt forurensende kjemikalier.",
+    link: "https://www.cleanbil.no/",
   },
   {
     image: "/images/showcase/intro.png",
-    title: "Intro",
-    visitTitle: "intro",
-    location: "Eksempel",
+    title: "Ale Sales Group AS",
+    visitTitle: "ASG",
+    location: "Strømsveien 258, Oslo",
     description:
-      "Nettside jeg lagde for å prøve meg på animasjoner og litt mer styling. Ikke en profesjonell nettside, heller et lite hobbyprosjekt.",
-    link: "https://nikolaithecreator.com",
+      "Ale Sales Group jobber med ulike salgs- og markedsaktiviteter mot sluttkunde. De har lang erfaring innen salg og markedsføring med fokus på å skape gunstige resultater innen forretningsområdene telemarketing, dørsalg og promotering.",
+    link: "https://www.alesalesgroup.no/",
+    // image: "/images/showcase/intro.png",
+    // title: "Intro",
+    // visitTitle: "intro",
+    // location: "Eksempel",
+    // description:
+    //   "Nettside jeg lagde for å prøve meg på animasjoner og litt mer styling. Ikke en profesjonell nettside, heller et lite hobbyprosjekt.",
+    // link: "https://nikolaithecreator.com",
   },
 ];
